4db9082128436b300c825c41e65cad835fc5d2ab
sparkvilla <diego.villamaina@gmail.com>
2017-09-04 16:58:47 +0200

    Added use of instance folder

--- project/__init__.py
+++ project/__init__.py
@@ -7,5 +7,6 @@ from flask import Flask
 #### config ####
 ################
  
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('flask.cfg')
 from . import views

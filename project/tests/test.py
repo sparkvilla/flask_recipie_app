8a450f4a080d42b0b06930d51f379164b6a4a4a1
sparkvilla <diego.villamaina@gmail.com>
2017-09-04 17:09:48 +0200

    Changed family name for mine

--- project/tests/test.py
+++ project/tests/test.py
@@ -37,7 +37,7 @@ class ProjectTests(unittest.TestCase):
  
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
-        self.assertIn(b'Welcome to the Kennedy Family Recipe App!', response.data)
+        self.assertIn(b'Welcome to the Villamaina Family Recipe App!', response.data)
         self.assertIn(b'This site describes our favorite family recipes!', response.data)
         self.assertIn(b'Breakfast Recipes', response.data)
         self.assertIn(b'Lunch Recipes', response.data)

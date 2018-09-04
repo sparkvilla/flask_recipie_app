24f0bf54fd0c16550d1dd44b986787936b8a4cba
sparkvilla <diego.villamaina@gmail.com>
2018-09-04 09:50:13 +0200

    fixed autotest for test_recipies

--- project/tests/test_recipes.py
+++ project/tests/test_recipes.py
@@ -37,11 +37,11 @@ class ProjectTests(unittest.TestCase):
 
     def test_main_page(self):
         response = self.app.get('/', follow_redirects=True)
-        self.assertIn(b'Villamaina Family Recipes', response.data)
-        self.assertIn(b'Breakfast Recipes', response.data)
-        self.assertIn(b'Lunch Recipes', response.data)
-        self.assertIn(b'Dinner Recipes', response.data)
-        self.assertIn(b'Dessert Recipes', response.data)
+        self.assertIn(b'Villamaina Family Recipies', response.data)
+        self.assertIn(b'Breakfast', response.data)
+        self.assertIn(b'Lunch', response.data)
+        self.assertIn(b'Dinner', response.data)
+        self.assertIn(b'Dessert', response.data)
 
 
 if __name__ == "__main__":
